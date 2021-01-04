7730338cab784bbe50e031c039285d0ce082eafa
Ruzert <ruzertyugal@gmail.com>
2021-01-04 19:49:49 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,16 +1,34 @@
 %ECHO OFF
 
-facetracker -l 1
+facetrackerNeos -l 1
 
 echo Make sure that nothing is accessing your camera before you proceed.
 
+set cameraNum=0
 set /p cameraNum=Select your camera from the list above and enter the corresponding number:
+echo.
 
-facetracker -a %cameraNum%
+facetrackerNeos -a %cameraNum%
 
-set /p dcaps=Select your camera mode or -1 for default settings:
-set /p fps=Select the FPS:
+set dcaps=-1
+set /p dcaps=Select your camera mode or -1 for default settings. A lower resolution runs faster:
+echo.
 
-facetracker -c %cameraNum% -F %fps% -D %dcaps% -v 3 -P 1 --discard-after 0 --scan-every 0 --no-3d-adapt 1 --max-feature-updates 900
+echo Tracking model options.
+echo Higher numbers generally offer better tracking quality, but slower speed.
+echo.
+echo -1: Poorest quality tracking but great performance, blinking is disabled.
+echo  0: Blinking is enabled in the following models.
+echo  1: Better quality than 0, but slower speed.
+echo  2: Better quality than 1, but slower speed.
+echo  3: Best tracking quality of all the models, but also the slowest model.
+echo -3: Quality is between -1 and 0.
+echo -2: Quality is roughly like 1, but is faster. Recommended unless you want better tracking accuracy, in which case use model 2 or 3.
+
+set model=-2
+set /p model=Select the tracking model (default -2): 
+echo.
+
+facetrackerNeos -c %cameraNum% -D %dcaps% -m %model%
 
 pause
